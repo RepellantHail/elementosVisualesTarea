72f11b283aee22351018ae34178fb7116125fa23
RepellantHail <a20310068@ceti.mx>
2023-03-31 16:16:00 -0600

    Agredados Eventos Para autoText, checkbox Agregar una vista principal con solo un scroll de movies Agregar una paleta de colores y diseño Agregar un segmento que haga los botones de un estilo y usar el mismo prototipo de btn

--- app/src/main/java/com/example/elementosvisualestarea/MainActivity.kt
+++ app/src/main/java/com/example/elementosvisualestarea/MainActivity.kt
@@ -1,37 +1,52 @@
 package com.example.elementosvisualestarea
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.Button
-import android.widget.RadioButton
-import android.widget.RadioGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Toast
+import java.text.FieldPosition
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         //Se inicia el boton para abrir otra activity
         sendFilmActivity()
+
         //Se llena el autotextview y cuando se agrega un valor se muestra con un toast
         val autotextView = findViewById<AutoCompleteTextView>(R.id.edtNombreMovie)
+        autotextView.threshold = 1  //Set threshhold to 1
         autoTextAdd(autotextView)
         poblarAutotext(autotextView)
+        //Evento Cuando Se selecciona un  elemento
+        autoTextSelectItem(autotextView)
+
+        //CheckBox
+        checkInitialize()
+
 
         //Iniciar valor por defecto para radio group
         var radioGroup = findViewById<RadioGroup>(R.id.radioGStatus)
         radioGroup.check(R.id.radioNoVisto)
-
-
         //Submit Button
         btnEnviar(autotextView)
 
+    }
+
+    fun autoTextSelectItem(autotextView:AutoCompleteTextView){
+        autotextView.setOnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position) as String
+            Toast.makeText(this@MainActivity, "Selected item: $selectedItem", Toast.LENGTH_SHORT).show()
+            println("Pelicula seleccionada: $selectedItem")
+        }
     }
 
     fun abrirFilms(v:View){
@@ -43,10 +58,50 @@ class MainActivity : AppCompatActivity() {
     }
     }
 
+    fun checkInitialize(){
+        val checkbox1 = findViewById<CheckBox>(R.id.checkBox1)
+        val checkbox2 = findViewById<CheckBox>(R.id.checkBox2)
+        val checkbox3 = findViewById<CheckBox>(R.id.checkBox3)
+        val checkbox4 = findViewById<CheckBox>(R.id.checkBox4)
+        val checkbox5 = findViewById<CheckBox>(R.id.checkBox5)
+
+        val checkboxListener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            val checkboxText = buttonView.text
+            if (isChecked) {
+                // Do something when the checkbox is checked
+                Toast.makeText(this@MainActivity, "$checkboxText esta marcada", Toast.LENGTH_SHORT).show()
+            } else {
+                // Do something when the checkbox is unchecked
+                Toast.makeText(this@MainActivity, "$checkboxText desmarcada", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val checkBoxes = listOf(checkbox1, checkbox2, checkbox3,checkbox4,checkbox5)
+
+        // Set the OnLongClickListener event for each CheckBox instance
+        checkBoxes.forEach { checkBox ->
+            checkBox.setOnLongClickListener {
+                // Handle the long-click event here
+                Toast.makeText(this, "Long-clicked on ${checkBox.text}", Toast.LENGTH_SHORT).show()
+                true
+            }
+        }
+
+        checkbox1.setOnCheckedChangeListener(checkboxListener)
+        checkbox2.setOnCheckedChangeListener(checkboxListener)
+        checkbox3.setOnCheckedChangeListener(checkboxListener)
+        checkbox4.setOnCheckedChangeListener(checkboxListener)
+        checkbox5.setOnCheckedChangeListener(checkboxListener)
+    }
+
     fun autoTextAdd (autotextView:AutoCompleteTextView){//Funcion Para hacer las sugerencias al autocompletar
         autotextView.setOnEditorActionListener{ textView,i,keyEvent ->
             var handled = false
+
             if(i == EditorInfo.IME_ACTION_DONE){
+                //Esconder Teclado
+                val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(autotextView.windowToken, 0)
                 //Agregar evento cuando se agregue el nombre de una pelicula
                 toastMessage(textView.text.toString())
                 handled = true
