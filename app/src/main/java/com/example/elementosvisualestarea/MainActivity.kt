3ccfb38522b5b83264a7f5a8bd4829bd0c5f59c6
RepellantHail <a20310068@ceti.mx>
2023-03-31 14:40:46 -0600

    Mejora en el codigo y division en funciones haciendo el on create más legible Agregar una vista principal con solo un scroll de movies Agregar una paleta de colores y diseño Agregar un segmento que haga los botones de un estilo y usar el mismo prototipo de btn

--- app/src/main/java/com/example/elementosvisualestarea/MainActivity.kt
+++ app/src/main/java/com/example/elementosvisualestarea/MainActivity.kt
@@ -17,48 +17,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //Button to Movie List
-        var btnFilm: Button
-        btnFilm = findViewById(R.id.activity_main_btnFilm)
-        btnFilm.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(view: View?) {
-                if (view != null) {
-                    abrirFilms(view)
-                }
-            }
-        })
-
-
+        //Se inicia el boton para abrir otra activity
+        sendFilmActivity()
+        //Se llena el autotextview y cuando se agrega un valor se muestra con un toast
         val autotextView = findViewById<AutoCompleteTextView>(R.id.edtNombreMovie)
-        autotextView.setOnEditorActionListener{ textView,i,keyEvent ->
-            var handled = false
-            if(i == EditorInfo.IME_ACTION_DONE){
-                //Agregar evento cuando se agregue el nombre de una pelicula
-                handled = true
-            }
-            handled
-        }
-        // Get the array of movies
-        val movies = resources.getStringArray(R.array.Movies)
-        // Create adapter and add in AutoCompleteTextView
-        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, movies)
-        autotextView.setAdapter(adapter)
+        autoTextAdd(autotextView)
+        poblarAutotext(autotextView)
 
+        //Iniciar valor por defecto para radio group
         var radioGroup = findViewById<RadioGroup>(R.id.radioGStatus)
         radioGroup.check(R.id.radioNoVisto)
 
 
         //Submit Button
-        val button = findViewById<Button>(R.id.btnAdd)
-        if (button != null)
-        {
-            button ?.setOnClickListener(View.OnClickListener {
-                val enteredText = getString(R.string.submitted_lang) + " " + autotextView.getText()
-                //Toast.makeText(applicationContext,"Your response has been recorded", Toast.LENGTH_LONG).show() //This line just show a toast
-            Toast.makeText(this@MainActivity, enteredText, Toast.LENGTH_SHORT).show()
-            })
-        }
+        btnEnviar(autotextView)
 
     }
 
@@ -69,5 +41,54 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
     }
+    }
+
+    fun autoTextAdd (autotextView:AutoCompleteTextView){//Funcion Para hacer las sugerencias al autocompletar
+        autotextView.setOnEditorActionListener{ textView,i,keyEvent ->
+            var handled = false
+            if(i == EditorInfo.IME_ACTION_DONE){
+                //Agregar evento cuando se agregue el nombre de una pelicula
+                toastMessage(textView.text.toString())
+                handled = true
+            }
+            handled
+        }
+    }
+
+    fun poblarAutotext(autotextView:AutoCompleteTextView){
+        //Funcion que llena las sugerencias. Saca los datos de un arreglo de strings
+        // Get the array of movies
+        val movies = resources.getStringArray(R.array.Movies)
+        // Create adapter and add in AutoCompleteTextView
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, movies)
+        autotextView.setAdapter(adapter)
+    }
+
+    fun sendFilmActivity(){//Funcion que abre otra activity
+        //Button to Movie List
+        var btnFilm: Button
+        btnFilm = findViewById(R.id.activity_main_btnFilm)
+        btnFilm.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(view: View?) {
+                if (view != null) {
+                    abrirFilms(view)
+                }
+            }
+        })
+    }
+
+    fun btnEnviar(autotextView:AutoCompleteTextView){//Funcion para el boton de enviar
+        val button = findViewById<Button>(R.id.btnAdd)
+        if (button != null)
+        {
+            button ?.setOnClickListener(View.OnClickListener {
+                val enteredText = getString(R.string.submitted_lang) + " " + autotextView.getText()
+                toastMessage(enteredText)
+            })
+        }
+    }
+
+    fun toastMessage(message:String){
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
     }
 }
